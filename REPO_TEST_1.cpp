88efce146ac3bbdd5b74a5740a80f0c58656c7c0
DianaAvanesova <avanesovadiana8@gmail.com>
2025-03-16 22:28:31 +0300

    probniy commit whith sfml

--- REPO_TEST_1.cpp
+++ REPO_TEST_1.cpp
@@ -1,7 +1,23 @@
-﻿
+﻿#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-    std::cout << "SFML is shit!!!\n";
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Event event;
+
+    while (window.isOpen())
+    {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
